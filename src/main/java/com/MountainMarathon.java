c685fe1874adc399dba5c01b3f9b9c1e34e3380a
Alex <alexceithamer@gmail.com>
2023-11-12 22:32:21 -0600

    Finished solution
    ~Switched the FindBestRoute() solution to use a 2d array, which also indicates direction in the graph (up or down). Much cleaner solution and works correctly. Hasn't been throughouly tested - but works with the problem given
    +added comments for the solution to explain it

--- src/main/java/com/MountainMarathon.java
+++ src/main/java/com/MountainMarathon.java
@@ -39,25 +39,6 @@ public class MountainMarathon {
         numberOfNodes = Integer.parseInt(input[1]);
 
         CreateGraph(data);
-        //PrintGraph();
-    }
-
-    public void PrintGraph() {
-        for (Node i : graph) {
-            System.out.print("Node: " + i.getNumber());
-            if (i.hasEdge()) {
-                System.out.print(" | " + "Up: " + i.getUpNodeNumber() + " " + i.getUpWeight());
-            } else {
-                System.out.print(" | " + "Up: " + "null" + " " + "null");
-            }
-
-            if (i.hasTwoEdges()) {
-                System.out.print(" | " + "Down: " + i.getDownNodeNumber() + " " + i.getDownWeight());
-            } else {
-                System.out.print(" | " + "Down: " + "null" + " " + "null");
-            }
-            System.out.println();
-        }
     }
 
 
@@ -108,7 +89,7 @@ public class MountainMarathon {
                     else if (i == level - 1) {
                         currentNode.setEdgeAndWeight(0, graph[nodeIndex + level - 1], data[weightIndex++]);
                     }
-                    //othereise set both edges in collapsing structure
+                    //otherwise set both edges in collapsing structure
                     else {
                         currentNode.setEdgeAndWeight(0, graph[nodeIndex + level - 1], data[weightIndex++]);
                         currentNode.setEdgeAndWeight(1, graph[nodeIndex + level], data[weightIndex++]);
@@ -129,15 +110,14 @@ public class MountainMarathon {
 
     public int FindBestRoute() {
         //2d array keeps track of the weight we have to that node (node 1 = index 0)
-        int[] dp = new int[numberOfNodes]; // [node]
-        //keep track of the current direction, so that if we switch direction, we can add the turn penalty
-        int[] direction = new int[numberOfNodes]; // 0 = up, 1 = down
+        //the 0 slot indicates the best weight from the node pointing up at it.
+        int[][] dp = new int[numberOfNodes][2]; // [node][up/down]
         //initialize array since weight of 0 when starting at node 1 (which is index 0)
-        dp[0] = 0;
-        direction[0] = -1;
+        dp[0][0] = 0;
+        dp[0][1] = 0;
         for (int i = 1; i < dp.length; i++) {
-            dp[i] = Integer.MIN_VALUE;
-            direction[i] = -1;
+            dp[i][0] = Integer.MIN_VALUE;
+            dp[i][1] = Integer.MIN_VALUE;
         }
 
         //iterate through nodes
@@ -147,70 +127,53 @@ public class MountainMarathon {
             //assign best path so far to connected nodes
             if (currentNode.hasUpEdge()) {
                 int upNodeIndex = currentNode.getUpNodeNumber() - 1;
-                //update weight of next node depending on if we are going up or down and if our current path is better
-                if (dp[i] + currentNode.getUpWeight() > dp[upNodeIndex] && direction[i] != 1) {
-                    //current node is going up, so no penalty for continuing upward direction
-                    dp[upNodeIndex] = dp[i] + currentNode.getUpWeight();
-                    direction[upNodeIndex] = 0;
+
+                /*
+                 * Choose the best path to the next node (calculating it with the penalty if we turn)
+                 * We check for MIN_VALUE because that indicates there is no path - and if we don't
+                 * check this special case, we get integer overflow on the negative side
+                 */
+                if (dp[i][0] == Integer.MIN_VALUE) {
+                    //we are assigning value to node up from us, but path came from down, so add penalty
+                    dp[upNodeIndex][0] = dp[i][1] + currentNode.getUpWeight() + turnPenalty;
                 }
-                else if (dp[i] + currentNode.getUpWeight() + turnPenalty > dp[upNodeIndex] && direction[i] == 1) {
-                    //current node is going down, so penalty for switching directions
-                    dp[upNodeIndex] = Math.max(dp[upNodeIndex], dp[i] + currentNode.getUpWeight() + turnPenalty);
-                    direction[upNodeIndex] = 0;
-                    
+                else if (dp[i][1] == Integer.MIN_VALUE) {
+                    //assigning value to node up from us, path continues up so no penalty
+                    dp[upNodeIndex][0] = dp[i][0] + currentNode.getUpWeight();
+                }
+                else {
+                    //Assign value accordingly (same as the 2 above if statements, but choosing the max value path)
+                    dp[upNodeIndex][0] = Math.max(dp[i][0] + currentNode.getUpWeight(), 
+                                                dp[i][1] + currentNode.getUpWeight() + turnPenalty);
                 }
             }
             if (currentNode.hasDownEdge()) {
                 int downNodeIndex = currentNode.getDownNodeNumber() - 1;
-                //update weight of next node depending on if we are going up or down and if our current path is better
-                if (dp[i] + currentNode.getUpWeight() > dp[downNodeIndex] && direction[i] != 0) {
-                    //current node is going down, so no penalty for continuing downward direction
-                    dp[downNodeIndex] = dp[i] + currentNode.getDownWeight();
-                    direction[downNodeIndex] = 1;
+/*
+                 * Choose the best path to the next node (calculating it with the penalty if we turn)
+                 * We check for MIN_VALUE because that indicates there is no path - and if we don't
+                 * check this special case, we get integer overflow on the negative side
+                 */                
+                if (dp[i][1] == Integer.MIN_VALUE) {
+                    //we are assigning value to node down form us, but path came from up, so add penalty
+                    dp[downNodeIndex][1] = dp[i][0] + currentNode.getDownWeight() + turnPenalty;
                 }
-                else if (dp[i] + currentNode.getDownWeight() + turnPenalty > dp[downNodeIndex] && direction[i] == 0) {
-                    //current node is going up, so penalty for switching directions
-                    dp[downNodeIndex] = Math.max(dp[downNodeIndex], dp[i] + currentNode.getDownWeight() + turnPenalty);
-                    direction[downNodeIndex] = 1;
+                else if (dp[i][0] == Integer.MIN_VALUE) {
+                    //assigning value to node down from us, path continues down so no penalty
+                    dp[downNodeIndex][1] = dp[i][1] + currentNode.getDownWeight();
+                }
+                else {
+                    //Assign value accordingly (same as the 2 above if statements, but choosing the max value path)
+                    dp[downNodeIndex][1] = Math.max(dp[i][1] + currentNode.getDownWeight(), 
+                                                    dp[i][0] + currentNode.getDownWeight() + turnPenalty);
                 }
                 
             }
         }
-        return dp[numberOfNodes - 1];
+        //at end of node, return the max of the path that comes from above or below - that is the solution
+        return Math.max(dp[numberOfNodes - 1][0], dp[numberOfNodes - 1][1]);
 
     }
-    /* 
-    // This method should contain the algorithm to find the best route
-    public int FindBestRoute() {
-        int[][] dp = new int[numberOfNodes][2]; // [node][up/down
-
-        //initialize dp array with base cases (0 because thats what we start off with)
-        dp[0][0] = 0;
-        dp[0][1] = 0;
-        
-        for (int i = 0; i < numberOfNodes; i++) {
-            Node currentNode = graph[i];
-            int upWeight = currentNode.getUpWeight();
-            int downWeight = currentNode.getDownWeight();
-
-            if (currentNode.hasUpEdge()) {
-                //upNodeIndex is set to the number of the next node, which when putting into an array
-                //it should subtract 1 to put into the array. 
-                int upNodeIndex = currentNode.getUpNode() - 1;
-                dp[upNodeIndex][0] = Math.max(dp[upNodeIndex][0], dp[i][0] + upWeight);
-                dp[upNodeIndex][1] = Math.max(dp[upNodeIndex][1], dp[i][1] + upWeight + turnPenalty);
-            }
-
-            if (currentNode.hasDownEdge()) {
-                int downNodeIndex = currentNode.getDownNode() - 1;
-                dp[downNodeIndex][0] = Math.max(dp[downNodeIndex][0], dp[i][0] + downWeight + turnPenalty);
-                dp[downNodeIndex][1] = Math.max(dp[downNodeIndex][1], dp[i][1] + downWeight);
-            }
-
-        }
-        return Math.max(dp[numberOfNodes - 1][0], dp[numberOfNodes - 1][1]);
-    } 
-    */
 
    
 
